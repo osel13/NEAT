cf4ebed0c88481bee4412fabce10e055974e040b
osel13 <Honzzaher@seznam.cz>
2025-02-22 13:07:38 +0100

    Scratch matrix

--- Scratch.cpp
+++ Scratch.cpp
@@ -8,10 +8,24 @@
 
 std::string kernel_code =
     "       void kernel func(global const float* A, global const float* B, global float* C){"
-    "       C[get_global_id(0)]=pow(A[get_global_id(0)],B[get_global_id(0)]);          "
-    "   }                                                                                   ";
+    "       int mw = 10;" //matrix width
+    //"       int row = get_global_id(0);" //matrix filled row
+    //"       int col = get_global_id(1);" //matrix filled column
+    //"       int aindex = row;" //a[i]
+    //"       int bindex = col;" //b[i]
+    "       int cindex;"// = row*mw + col;" //c[i][j]
+    "       float cij;" //matrix val
+    "       for(int i = get_global_id(0);i<mw;i++){"
+    "       for(int j = get_global_id(1);j<mw;j++){"
+    "       cindex = i*mw + j;"
+    "       cij = A[i]*B[j];"   
+    "       C[cindex] = cij;"
+    "       }"
+    "       }"
+    "    }";
     /*"   void kernel simple_add(global const int* A, global const int* B, global int* C){ "
-    "       C[get_global_id(0)]=A[get_global_id(0)]+B[get_global_id(0)];                 "
+    "       C[get_global_id(0)]=A[get_global_id(0)]+B[get_global_id(0)];                   "
+    "        C[get_global_id(0)]=pow(A[get_global_id(0)],B[get_global_id(0)]);  
     "   }                                                                               ";*/
 int main(int arg, char* args[])
 {
@@ -44,18 +58,18 @@ int main(int arg, char* args[])
     
     cl::Buffer A_d(context, CL_MEM_READ_WRITE, sizeof(float) * SIZE);
     cl::Buffer B_d(context, CL_MEM_READ_WRITE, sizeof(float) * SIZE);
-    cl::Buffer C_d(context, CL_MEM_READ_WRITE, sizeof(float) * SIZE);
+    cl::Buffer C_d(context, CL_MEM_READ_WRITE, sizeof(float) * SIZE*SIZE);//sizeof(float) * SIZE);
     
     //Allocating memory -> Buffer
     //*_h = host
     //*_d = device
     
-    float A_h[] = { 0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0 };
+    float A_h[] = { 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0 };
     float B_h[] = { 10.0, 9.0, 8.0, 7.0, 6.0, 5.0, 4.0, 3.0, 2.0, 1.0 };
     
     //Queue
     cl::CommandQueue queue(context, default_device);
-    
+
     //Writing into device memory
     queue.enqueueWriteBuffer(A_d, CL_TRUE, 0, sizeof(float) * SIZE, A_h);
     queue.enqueueWriteBuffer(B_d, CL_TRUE, 0, sizeof(float) * SIZE, B_h);
@@ -84,18 +98,20 @@ int main(int arg, char* args[])
         func(cl::EnqueueArgs(queue,global), A_d, B_d, C_d).wait();
         
     //Kernel execution on the device
-        float C_h[SIZE];
+        float C_h[SIZE*SIZE];
         
     //Retrieving data from device
-        queue.enqueueReadBuffer(C_d, CL_TRUE, 0, sizeof(int) * SIZE, C_h);
+        queue.enqueueReadBuffer(C_d, CL_TRUE, 0, sizeof(float) * SIZE*SIZE, C_h);
         
     //read result
         std::cout << " result:" <<std::endl;
-    for (int i = 0; i<10; i++) {
-        std::cout << C_h[i] << " ";
-    }   
-    
+    for (int i = 0; i<SIZE; i++) {
+        for (int j = 0; j<SIZE; j++) {
+            int k = i*SIZE + j;
+        std::cout << C_h[k] << " ";
+        }   
         std::cout << std::endl;
+    }
     
     return 0;
 }
