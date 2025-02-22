bd0a0bd23e420743b2ace5ca951dd538f9ab99bb
Andy Black <Sprotex@gmail.com>
2025-02-22 21:27:53 +0100

    add random utils

---
+++ utils.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <random>
+#include <iterator>
+
+static std::mt19937 default_generator(std::random_device{}());
+
+template<typename Iterator, typename RandomGenerator>
+Iterator random_from_range(Iterator start, Iterator end, RandomGenerator& random) {
+	auto distance = std::distance(start, end);
+    std::uniform_int_distribution<> distribution(0, std::distance(start, end) - 1);
+    std::advance(start, distribution(random));
+    return start;
+}
+
+template <typename Type, typename RandomGenerator>
+const Type& random_thresholded_pick(float threshold, const Type& a, const Type& b, RandomGenerator& random) {
+    std::uniform_int_distribution<> distribution(0, std::distance(start, end) - 1);
+	const auto random_value = distribution(random);
+    return random_value <= threshold ? a : b;
+}
+
+template <typename Type>
+const Type& random_thresholded_pick(float threshold, const Type& a, const Type& b) {
+	return random_thresholded_pick(threshold, a, b, default_generator);
+}
